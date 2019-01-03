21f39a1a9732acc3e1445d507b870b7a930f587e
Balazs Bohonyi <balazs.bohonyi@gmail.com>
2019-01-03 14:40:50 +0200

    Added GET /todos route

--- server/server.js
+++ server/server.js
@@ -26,6 +26,16 @@ app.post('/todos', (req, res) => {
   });
 });
 
+// route to get all the Todos
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos})
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+// starting the server
 app.listen(3000, () => {
   console.log('Started on port 3000')
 });
